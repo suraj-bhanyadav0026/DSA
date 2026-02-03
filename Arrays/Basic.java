3f55df917071aba0287a8a49ca93a743ce286209
Suraj Bhan <surajbhan@Surajs-MacBook-Air.local>
2026-02-03 16:01:58 +0530

    printing full array

--- Arrays/Basic.java
+++ Arrays/Basic.java
@@ -6,6 +6,27 @@ public class Basic {
         System.out.println(student[1]);
         student [2] = 49;
         System.out.println(student[2]);
+
+        int [] student1 = new int [4];
+        student1[0] = 43;
+        student1[1] = 75;
+        student1[2] = -23;
+        student1[3] = 98;
+
+        System.out.println(student1[2]);
+        System.out.println(student1[3]);
+        //but if we want to print whole array for that-
+        for (int i =0; i<= 3;  i++){
+                System.out.print(student1[i]+" ");
+            }
+            System.out.println();
+            int i =0;
+
+        while (i<= 3){
+            System.out.print(student1[i]+" ");
+            i++;
+        }
+         
     }
     
 }
