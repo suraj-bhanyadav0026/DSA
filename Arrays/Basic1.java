c00b8cc439d9ede08467a91f4f744af3dbfcdf81
Suraj Bhan <surajbhan@Surajs-MacBook-Air.local>
2026-02-04 18:52:21 +0530

    inputing element in array by taking input of it

---
+++ Arrays/Basic1.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Basic1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[7];
+
+        for (int j = 0; j < 7; j++) {
+            System.out.print("Enter the " + j + "th number to store it in the array: ");
+            arr[j] = sc.nextInt();
+        }
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < 7; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
